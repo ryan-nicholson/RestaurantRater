017a680643566f97562476fa2208d76fd3064eb2
Ryan Nicholson <rbnich2012@gmail.com>
2020-02-25 13:37:30 -0500

    worked on

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        // POST
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
+        {
+            if (ModelState.IsValid && restaurant != null)
+            {
+                _context.Restaurants.Add(restaurant);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
