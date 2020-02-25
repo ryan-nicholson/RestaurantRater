fe703f15de2f47fd6d25c403221b9deeff333727
Ryan Nicholson <rbnich2012@gmail.com>
2020-02-25 10:47:45 -0500

    Add project files.

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Style { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0d, 5d)]
+        public double rating { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int DollarSigns { get; set; }
+    }
+}
